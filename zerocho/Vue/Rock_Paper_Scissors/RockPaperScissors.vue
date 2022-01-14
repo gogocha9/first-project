dd2206039d2528f3f2ab12add4ae7de1529b193f
YoungHyun Cha <gogocha9@naver.com>
2022-01-14 17:40:10 +0900

    commit

--- zerocho/Vue/Rock_Paper_Scissors/RockPaperScissors.vue
+++ zerocho/Vue/Rock_Paper_Scissors/RockPaperScissors.vue
@@ -17,6 +17,19 @@
         가위: '-90px',
         보: '-175px',
     }
+
+    const scores = {
+        가위: 1,
+        바위: 0,
+        보: -1,
+    }
+    const computerChoice = (imgCord) => {
+        return Object.entries(rspCord).find((v) => {
+            return v[1] === imgCord;
+        })[0];
+    };
+
+    let interval = null;
     export default {
         data() {
             return {
@@ -34,11 +47,64 @@
         },
 
         methods: {
+            changeHand() {
+                interval = setInterval(() => {
+                if(this.imgCord === rspCord.바위) {
+                    this.imgCord = rspCord.가위;
+                } else if(this.imgCord === rspCord.가위) {
+                    this.imgCord = rspCord.보;
+                } else if(this.imgCord === rspCord.보) {
+                    this.imgCord = rspCord.바위;
+                }
+            }, 100);
+            },
             onClickButton(choice) {
-
-            }
-        }
+                clearInterval(interval);
+                const myScore = scores[choice];
+                const cpuScore = scores[computerChoice(this.imgCord)];
+                const diff = myScore - cpuScore;
+                if(diff === 0) {
+                    this.result = '비겼습니다.';
+                } else if([-1, 2].includes(diff)) {
+                    this.result = '이겼습니다';
+                    this.score += 1;
+                } else {
+                    this.result = '졌습니다';
+                    this.score -= 1;
+                }
+                setTimeout(() => {
+                    this.changeHand()
+                }, 1000);
+            },
+        },
+        beforeCreate() {
+            console.log('beforeCreate');
+        },
+        create() {
+            console.log('create');
+        },
+        beforeMount() {
+            console.log('beforeMount');
+        },
+        mounted() {
+            console.log('mounted');
+            this.changeHand();
+        },
+        beforeUpdate() {
+            console.log('beforeUpdate');
+        },
+        update() {
+            console.log('update');
+        },
+        beforeDestroyed() {
+            console.log('beforeDestroyed');
+            clearInterval(interval);
+        },
+        destroyed() {
+            console.log('destroyed');
+        },
     };
+
 </script>
 
 <style scoped>
