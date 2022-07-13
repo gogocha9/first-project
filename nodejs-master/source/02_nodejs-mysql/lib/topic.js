b1db86a3d27276a1ca8a378ec3c582d0545e3eb2
YoungHyun Cha <gogocha9@naver.com>
2022-07-14 02:12:14 +0900

    commit_220714

--- nodejs-master/source/02_nodejs-mysql/lib/topic.js
+++ nodejs-master/source/02_nodejs-mysql/lib/topic.js
@@ -1,7 +1,8 @@
-var gs = require('querystring');
+var qs = require('querystring');
 var url = require('url');
 var db = require('./db');
 var template = require('./template.js');
+
 exports.home = (request, response) => {
     db.query(`SELECT * FROM topic`, (error, topics) => {
         var title = 'Welcome';
@@ -18,7 +19,7 @@ exports.home = (request, response) => {
 
 exports.page = (request, response) => {
     var _url = request.url;
-    var queryData = url.paarse(_url, true).query;
+    var queryData = url.parse(_url, true).query;
     db.query(`SELECT * FROM topic`, (error, topics) => {
         if(error) {
             throw error;
@@ -81,5 +82,92 @@ exports.create = (request, response) => {
 }
 
 exports.create_process = (request, response) => {
+    var body = '';
+    request.on('data', (data) => {
+        body += data;
+    });
+    request.on('end', () => {
+        var post = qs.parse(body);
+        db.query(`
+            INSERT INTO topic (title, description, created, author_id)
+                VALUES(?, ?, NOW(), ?)`,
+            [post.title, post.description, post.author], (error, result) => {
+                if(error) {
+                    throw error;
+                }
+                response.writeHead(302, {Location: `/?id=${result.insertId}`});
+                response.end();
+            }
+        );
+    });
+}
 
+exports.update = (request, response) => {
+    var _url = request.url;
+    var queryData = url.parse(_url, true).query;
+    db.query('SELECT * FROM topic', (error, topics) => {
+        if(error) {
+            throw error;
+        }
+        db.query('SELECT * FROM topic WHERE id=?', [queryData.id], (error2, topic) => {
+            if(error2) {
+                throw error2;
+            }
+            db.query('SELECT * FROM author', (error2, authors) => {
+                var list = template.list(topics);
+                var html = template.HTML(topic[0].title, list,
+                    `
+                    <form action="/update_process" method="post">
+                        <input type="hidden" name="id" value="${topic[0].id}">
+                        <p><input type="text" name="title" placeholder="title" value="${topic[0].title}"></p>
+                        <p>
+                            <textarea name="description" placeholder="description">${topic[0].description}</textarea>
+                        </p>
+                        <p>
+                            ${template.authorSelect(authors, topic[0].author_id)};
+                        </p>
+                        <p>
+                            <input type="submit">
+                        </p>
+                    </form>
+                    `,
+                    `<a href="/create">create</a> <a href="/update?id=${topic[0].title}">update</a>`
+                );
+                response.writeHead(200);
+                response.end(html);
+            });
+        });
+    });
+}
+
+exports.update_process = (request, response) => {
+    var body = '';
+    request.on('data', (data) => {
+        body += data;
+    });
+    request.on('end', () => {
+        var post = qs.parse(body);
+        db.query('UPDATE topic SET title=?, description=?, author_id=? WHERE id=?',
+        [post.title, post.description, post.author, post.id], (error, result) => {
+            response.writeHead(302, {Location: `/?id=${post.id}`});
+            response.end();
+        });
+    });
+}
+
+exports.delete_process = (request, response) => {
+    var body = '';
+    request.on('data', (data) => {
+        body += data;
+    });
+    request.on('end', () => {
+        var post = qs.parse(body);
+        db.query('DELETE FROM topic WHERE id = ?', [post.id], (error, result) => {
+            if(error) {
+                throw error;
+            }
+            response.writeHead(302, {Location: '/'});
+            response.end();
+        });
+    });
 }
