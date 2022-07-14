5304a3b6bba6684b0c9ee639a495c56da7776063
chayounghyun <gogocha9@naver.com>
2022-07-14 18:08:16 +0900

    commit_220714

--- nodejs-master/source/02_nodejs-mysql/main.js
+++ nodejs-master/source/02_nodejs-mysql/main.js
@@ -1,10 +1,9 @@
 console.log('Hello no deamon');
 var http = require('http');
 var url = require('url');
-var qs = require('querystring');
-var template = require('./lib/template.js');
-var db = require('./lib/db');
+const { authorSelect } = require('./lib/template');
 var topic = require('./lib/topic');
+var author = require('./lib/author');
 
 var app = http.createServer(function(request, response) {
     var _url = request.url;
@@ -27,6 +26,19 @@ var app = http.createServer(function(request, response) {
         topic.update_process(request, response);
     } else if(pathname === '/delete_process') {
         topic.delete_process(request, response);
+    } else if(pathname === '/author') {
+        author.home(request, response);
+    } else if(pathname === '/author/create_process') {
+        author.create_process(request, response);
+    } else if(pathname === '/author/update') {
+        author.update(request, response);
+    } else if(pathname === '/author/update_process') {
+        author.update_process(request, response);
+    } else if(pathname === '/author/delete_process') {
+        author.delete_process(request, response);
+    } else {
+        response.writeHead(404);
+        response.end('Not found');
     }
 });
 app.listen(3000);
